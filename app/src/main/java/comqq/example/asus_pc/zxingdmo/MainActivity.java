030be6c682f8b1845e842a07e5fdf42053e6c6b0
JunRen200 <852092058@qq.com>
2017-04-17 20:11:02 +0800

    Initial commit

---
+++ app/src/main/java/comqq/example/asus_pc/zxingdmo/MainActivity.java
@@ -0,0 +1,110 @@
+package comqq.example.asus_pc.zxingdmo;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+import java.io.UnsupportedEncodingException;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnCreate;
+    private ImageView imgResult;
+    private Button btn_open;
+    private TextView txt;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnCreate= (Button) findViewById(R.id.btnCreate);
+        imgResult= (ImageView) findViewById(R.id.img_result);
+        btn_open= (Button) findViewById(R.id.btn_open);
+        txt= (TextView) findViewById(R.id.txt);
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s="你你还活着吗";
+                try {
+                    s=new String(s.getBytes("UTF-8"),"ISO-8859-1");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                imgResult.setImageBitmap(encodeAsBitmap(s));
+            }
+        });
+        btn_open.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                customScan();
+            }
+        });
+
+    }
+    Bitmap encodeAsBitmap(String str){
+        Bitmap bitmap = null;
+        BitMatrix result = null;
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        try {
+            result = multiFormatWriter.encode(str, BarcodeFormat.QR_CODE, 200, 200);
+            // 使用 ZXing Android Embedded 要写的代码
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            bitmap = barcodeEncoder.createBitmap(result);
+        } catch (WriterException e){
+            e.printStackTrace();
+        } catch (IllegalArgumentException iae){ // ?
+            return null;
+        }
+
+        // 如果不使用 ZXing Android Embedded 的话，要写的代码
+
+//        int w = result.getWidth();
+//        int h = result.getHeight();
+//        int[] pixels = new int[w * h];
+//        for (int y = 0; y < h; y++) {
+//            int offset = y * w;
+//            for (int x = 0; x < w; x++) {
+//                pixels[offset + x] = result.get(x, y) ? BLACK : WHITE;
+//            }
+//        }
+//        bitmap = Bitmap.createBitmap(w,h,Bitmap.Config.ARGB_8888);
+//        bitmap.setPixels(pixels,0,100,0,0,w,h);
+
+        return bitmap;
+    }
+    public void customScan(){
+        new IntentIntegrator(this)
+                .setOrientationLocked(false)
+                .setCaptureActivity(CustomScanActivity.class) // 设置自定义的activity是CustomActivity
+                .initiateScan(); // 初始化扫描
+    }
+    @Override
+// 通过 onActivityResult的方法获取 扫描回来的 值
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        if(intentResult != null) {
+            if(intentResult.getContents() == null) {
+                Toast.makeText(this,"内容为空",Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this,"扫描成功",Toast.LENGTH_LONG).show();
+                // ScanResult 为 获取到的字符串
+                String ScanResult = intentResult.getContents();
+                txt.setText(ScanResult);
+            }
+        } else {
+            super.onActivityResult(requestCode,resultCode,data);
+        }
+    }
+}
